c806bfcdd01b26630eaf7d2084c119ec49dd3afa
Luis Correa <alberto13711@gmail.com>
2019-05-22 07:52:01 +0200

    Avance 1

--- src/router.js
+++ src/router.js
@@ -2,10 +2,11 @@ import Home from './components/Home.vue';
 import Register from './components/Register.vue';
 import Login from './components/Login.vue';
 
+
 const routes = [
-    { path: '/', component: Home },
-    { path: '/register', component: Register },
-    { path: '/login', component: Login },
+    { path: '/', component: Home, name: 'home' },
+    { path: '/register', component: Register, name: 'register' },
+    { path: '/login', component: Login, name: 'login' },
 ];
 
 export default routes;
