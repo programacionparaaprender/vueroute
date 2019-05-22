8410582efd1db9c050489dd0395db920625e8216
Luis Correa <alberto13711@gmail.com>
2019-05-22 07:14:18 +0200

    Practicas de vue

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+        <nav class="row">
+            <router-link class="row" style="height:30px;background-color:orangered;" to='/'>
+            Home
+            </router-link>
+            <router-link class="row" style="height:30px;background-color:orangered;" to='/register'>
+            Register
+            </router-link>
+            <router-link class="row" style="height:30px;background-color:orangered;" to='/login'>
+            Login
+            </router-link>
+        </nav>
+        <router-view />
+    </div>
+</template>
+<style>
+    .row
+    {
+        width:100%;
+        float:left;
+    }
+    #app
+    {
+        width:100%;
+        height:100%;
+    }
+</style>
+    <script >
+
+    export default {
+    }
+
+    </script >
