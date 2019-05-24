f38951d2865d479fc5c9313c2e81a6476789afee
Luis Correa <alberto13711@gmail.com>
2019-05-24 21:26:13 +0200

    Integrando Axios

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,36 @@
 <template>
     <div>
+        <div>
         Home
+        </div>
+        <div>
+        <input type="button" value="Boton" v-on:click="boton()" />
+        </div>
     </div>
 </template>
 <script>
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+ 
+Vue.use(VueAxios, axios);
+var api = "http://www.programandoconrupert.com/ws/servicio-gatos.php";
 export default {
-
+    methods:{
+        boton:function(){
+            Vue.axios.get(api).then((response) => {
+                var json = JSON.stringify(response.data);
+            console.log(json)
+            })
+            
+            this.axios.get(api).then((response) => {
+            console.log(response.data)
+            })
+            
+            this.$http.get(api).then((response) => {
+            console.log(response.data)
+            })
+        }
+    }
 }
 </script>
