34bfccd69519ce278c7c28bcd17414a68cc29670
Luis Correa <yancel209@gmail.com>
2020-09-05 07:49:51 -0500

    cambios

---
+++ src/components/computed/EjemploComputed.vue
@@ -0,0 +1,32 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            firstName:"",
+            lastName:""
+        }
+    },
+    computed: {
+        fullName: {
+            // getter
+            get: function () {
+            return this.firstName + ' ' + this.lastName
+            },
+            // setter
+            set: function (newValue) {
+            var names = newValue.split(' ')
+            this.firstName = names[0]
+            this.lastName = names[names.length - 1]
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
