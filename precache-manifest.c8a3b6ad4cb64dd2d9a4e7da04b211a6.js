0a670dabc0405327078ba7e76c7aaf7f4f22bac4
Luis Correa <yancel209@gmail.com>
2021-02-14 12:50:13 -0500

    deploy

---
+++ precache-manifest.c8a3b6ad4cb64dd2d9a4e7da04b211a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d8b4e78f63230d005f",
+    "url": "/vueroute/css/app.81ec6f6e.css"
+  },
+  {
+    "revision": "d021663cafe05a6ad71f",
+    "url": "/vueroute/css/chunk-vendors.01827b2c.css"
+  },
+  {
+    "revision": "1ca7838f67f8ed90586e5b51d693c17e",
+    "url": "/vueroute/index.html"
+  },
+  {
+    "revision": "f8d8b4e78f63230d005f",
+    "url": "/vueroute/js/app.c896af10.js"
+  },
+  {
+    "revision": "d021663cafe05a6ad71f",
+    "url": "/vueroute/js/chunk-vendors.bbba29d5.js"
+  },
+  {
+    "revision": "0f0afdf0712f9624189819f448ff5df5",
+    "url": "/vueroute/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vueroute/robots.txt"
+  }
+]);
